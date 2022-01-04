353271bf5f1215d85768bd6f74c8081e04ee4e16
Erkan Yalçın <erkyalcin@gmail.com>
2022-01-04 18:43:57 +0300

    updates

---
+++ codility/BinaryGap.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int solution(int N) {
+
+        int maxGap = 0;
+        int gap = -1;
+        while (true) {
+            if (N % 2 == 1) {
+                maxGap = maxGap < gap ? gap : maxGap;
+                N = N - 1;
+                if (N == 0) {
+                    break;
+                }
+                gap = 0;
+            } else {
+                if (gap != -1)
+                    gap++;
+            }
+            N = N / 2;
+        }
+        return maxGap;
+    }
+}
---
+++ codility/CyclicRotation.java
@@ -0,0 +1,20 @@
+package codility;
+
+class Solution {
+    public int[] solution(int[] A, int K) {
+       
+       int [] res = new int [A.length];
+
+       K = K % A.length;
+       if(K == 0){
+           return A;
+       }
+
+       for( int i = 0;i<A.length;i++){
+           res[(i+K) % A.length ] = A[i];
+       }
+       
+       return res;
+    }
+}
+
